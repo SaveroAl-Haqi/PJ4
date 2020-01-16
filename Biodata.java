1b7d0f3d751b72b0580e352055261f1348500777
SaveroAl-Haqi <yusaalhaqi22@gmail.com>
2020-01-16 08:34:23 +0700

    test

--- Biodata.java
+++ Biodata.java
@@ -9,6 +9,8 @@ public class Biodata {
         String Nim;
         String Kelas;
         String Alamat;
+	String Gender;
+	String NoTelepon;
         //memberikan nilai pada variable secara dinamis
         //memanggil class scanner
         Scanner input_bio=new Scanner (System.in);
@@ -24,11 +26,19 @@ public class Biodata {
         //Alamat
         System.out.print("Alamat:");
         Alamat="Jakarta";
+	//Gender
+        System.out.print("Gender:");
+        Gender="Laki";
+	//NoTelepon
+        System.out.print("NoTelepon:");
+        NoTelepon="021000001";
         //cetak nama, nim, kelas, dan alamat
         System.out.println("Nama"+Nama);
         System.out.println("Nim"+Nim);
         System.out.println("Kelas"+Kelas);
         System.out.println("Alamat"+Alamat);
+	System.out.println("Gender"+Gender);
+	System.out.println("NoTelepon"+NoTelepon);
     }
     
 }
